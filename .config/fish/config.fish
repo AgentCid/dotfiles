d317676be67cac6563f5b22b114b4ee56de59c05
Austin Whittington <austin_whitt@yahoo.com>
2016-08-04 05:46:27 -0500

    updated for auto X login, backlight controls, and wallpaper

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,8 @@
 set PATH /usr/local/sbin /usr/local/bin /usr/bin /usr/lib/jvm/default/bin /usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl
+
+# Start X at login
+if status --is-login
+  if test -z "$DISPLAY" -a $XDG_VTNR = 1
+    exec startx -- -keeptty
+  end
+end
