6ce10fd671ebf4116aec814c2045fed305da707a
Austin Whittington <austin_whitt@yahoo.com>
2018-05-08 12:16:13 -0500

    added new plugins

--- scripts/vim_setup.sh
+++ scripts/vim_setup.sh
@@ -2,10 +2,12 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
-git clone https://github.com/w0ng/vim-hybrid.git
+git clone https://github.com/itchyny/lightline.vim
 git clone https://github.com/jlanzarotta/bufexplorer.git
-git clone https://github.com/xolox/vim-session.git
+git clone https://github.com/tpope/vim-surround.git
+git clone https://github.com/w0ng/vim-hybrid.git
 git clone https://github.com/xolox/vim-misc.git
+git clone https://github.com/xolox/vim-session.git
 
 mkdir -p ~/.vim/undodir
 mkdir -p ~/.vim/tmpdir
